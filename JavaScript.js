25a1d25afc0421c65a9154b59729c61bb44b6930
João Victor Pires Dos Santos <54391360+Jv-Pi@users.noreply.github.com>
2021-01-07 22:31:55 +0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    menuToggle.classList.toggle('active');
+    banner.classList.toggle('active');
+};
+
+window.onscroll = function() {
+    scroll();
+};
+
+function scroll() {
+    let btn = document.getElementById("btntop");
+    if (document.documentElement.scrollTop > 50) {
+        btn.style.display = "block"
+    } else {
+        btn.style.display = "none"
+    }
+};
+
+function up() {
+    document.documentElement.scrollTop = 0;
+
+}
+
+newFunction();
+
+function newFunction() {
+    $(window).on("load", function() {
+        $(".loader-wrapper").fadeOut("slow");
+    });
+}
